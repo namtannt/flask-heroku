f83c8b88aceb43c1a650f8486a79ce080301c297
namtannt <89174741+namtannt@users.noreply.github.com>
2021-09-02 11:07:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello วิมลรัตน์ พรหมชาติ เลขที่26 ห้อง 4/5"
 
 @app.route('/api', methods=['GET'])
 def get_api():
